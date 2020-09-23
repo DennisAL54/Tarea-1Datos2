af65f115a47a39d24cc247109047b87cd44dbff4
Dennis <alejimenezc@hotmail.com>
2020-09-22 23:17:35 -0600

    Creacion del proyecto

---
+++ cmake-build-debug/CMakeFiles/Tarea_1Datos2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tarea_1Datos2.dir/main.cpp.o"
+  "Tarea_1Datos2"
+  "Tarea_1Datos2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tarea_1Datos2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
